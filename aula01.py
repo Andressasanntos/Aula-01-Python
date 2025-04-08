a0ac0731568fbc119b0ba840b05dfaee3f6edf34
Dressa_Dev <102365712+Andressasanntos@users.noreply.github.com>
2025-04-08 17:45:24 -0300

    Add files via upload

---
+++ aula01.py
@@ -0,0 +1,38 @@
+'''
+INTEIROS = TNT
+PALAVRAS/LETRAS = STRING
+FRACIONADO/FLUTUANREW = FLOAT
+'''
+
+def bom_dia():
+    print("Hello world!")
+    
+def saudacao(nome):
+    print(f"Olá, {nome}! Seja bem-vindo ao mundo do python")
+    nom = "Living"
+    
+a = "Andressa"    
+saudacao(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ correcaoaula.01.py
@@ -0,0 +1,29 @@
+'''
+INTEIROS = TNT
+PALAVRAS/LETRAS = STRING
+FRACIONADO/FLUTUANREW = FLOAT
+Soma fixa de 2+2
+SOMA, DIVISÃO, SUBTRAÇÃO , MULTIPLICAÇÃO
+'''
+
+
+
+def soma():
+    resultado = 2 + 2
+    print("O resultado é: {resultado}")
+    
+    def subtracao(a, b):
+        resultado = a - b
+    print(f"O resultado é: {resultado}")
+    
+    def multiplicao(a,b):
+        resultado = a * b
+    print(f"O resultado é: {resultado}")
+    
+    def divisao(a, b):
+        resultado = a / b
+    print(f"O resultado é: {resultado}")
+    
+    divisao (50, 7)
+    
+    multiplicao(9, 7)
